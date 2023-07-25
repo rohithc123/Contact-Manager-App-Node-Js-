a6af3a75a09bcd9f5aa2903b522c43b142564dab
Rohith C <rohithc.eec.cseb@gmail.com>
2023-07-25 20:38:51 +0530

    Checking if thunder client is working

--- contacts-backend/server.js
+++ contacts-backend/server.js
@@ -1,1 +1,14 @@
-console.log("I am in the express project")
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.get('/api/contacts', (req,res) =>{
+    res.send("Get all contacts");
+})
+
+app.listen(port, () =>{
+    console.log(`server running on port ${port}`)
+});
